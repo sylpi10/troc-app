70eb871a7747086f0341f783f2d16ac7ad2abcfe
sylpi <syl.pillet@hotmail.fr>
2022-09-01 16:47:16 +0200

    usercan add relaypoints

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\ObjectToSell;
+use App\Entity\RelayPoint;
 use App\Entity\Shop;
 use App\Form\ObjectType;
+use App\Form\RelayType;
 use App\Form\ShopType;
+use App\Repository\RelayPointRepository;
 use App\Repository\ShopRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -20,11 +23,17 @@ class AccountController extends AbstractController
     protected EntityManagerInterface $em;
     protected UserRepository $userRepo;
     protected ShopRepository $shopRepo;
-    public function __construct(EntityManagerInterface $em, UserRepository $userRepo, ShopRepository $shopRepo)
-    {
+    protected RelayPointRepository $relayRepo;
+    public function __construct(
+        EntityManagerInterface $em,
+        UserRepository $userRepo,
+        ShopRepository $shopRepo,
+        RelayPointRepository $relayRepo
+    ) {
         $this->em = $em;
         $this->userRepo = $userRepo;
         $this->shopRepo = $shopRepo;
+        $this->relayRepo = $relayRepo;
     }
 
     #[Route('/account', name: 'account')]
@@ -32,12 +41,15 @@ class AccountController extends AbstractController
     {
         $shop = new Shop();
         $objectToSell = new ObjectToSell();
+        $relaypoint = new RelayPoint();
 
         $shopForm = $this->createForm(ShopType::class, $shop)->handleRequest($request);
         $objectForm = $this->createForm(ObjectType::class, $objectToSell)->handleRequest($request);
+        $relayForm = $this->createForm(RelayType::class, $relaypoint)->handleRequest($request);
 
         $showShopForm = true;
         $userShop = $this->shopRepo->findOneBy(['user' => 9]);
+        $userRelays = $this->relayRepo->findBy(['user' => 9]);
         if ($this->getUser()->getId() == $userShop->getUser()->getId()) {
             $showShopForm = false;
         }
@@ -62,13 +74,23 @@ class AccountController extends AbstractController
 
             return $this->redirectToRoute("account");
         }
+        if ($relayForm->isSubmitted() && $relayForm->isValid()) {
+            $relaypoint->setUser($this->getUser());
+
+            $this->em->persist($relaypoint);
+            $this->em->flush();
+
+            return $this->redirectToRoute("account");
+        }
 
 
         return $this->render('account/account.html.twig', [
             "shopForm" => $shopForm->createView(),
             "objectForm" => $objectForm->createView(),
             "showShopForm" => $showShopForm,
-            "shop" => $userShop
+            "relayForm" => $relayForm->createView(),
+            "shop" => $userShop,
+            "relay" => $userRelays
         ]);
     }
 }
